ee7c118e810439acd5896bc03bb4b2b95e4671ff
Jenwng <Jenniescwong@gmail.com>
2020-01-04 22:41:55 -0500

    Updates

---
+++ precache-manifest.2ac13d8451d55b099999005529af8cf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d47b4e4ac438df7f7e63",
+    "url": "/me/static/css/main.2bf5c86a.chunk.css"
+  },
+  {
+    "revision": "d47b4e4ac438df7f7e63",
+    "url": "/me/static/js/main.d47b4e4a.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "bea9b55bf8e826654e9e10168320441c",
+    "url": "/me/index.html"
+  }
+];
